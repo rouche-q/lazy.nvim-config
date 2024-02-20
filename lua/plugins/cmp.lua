47c455ffc3bca657217da279f42a009df4cef918
rouche-q <17704187+rouche-q@users.noreply.github.com>
2024-02-20 13:40:51 +0100

    [fix]: better completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -27,16 +27,15 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = "luasnip" },
-      }, {
-        { name = "buffer" },
-        { name = "path" },
-        { name = "cmdline" },
-      }),
+          { name = 'nvim_lsp' },
+          { name = "luasnip" },
+        },
+        {
+          { name = "buffer" },
+        }),
     })
   end
 }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,9 +2,8 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
     require("neo-tree").setup({
