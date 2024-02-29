065ec47bb97db2a2d0dc877842a05e6a5c8ebc80
rouche-q <17704187+rouche-q@users.noreply.github.com>
2024-02-29 16:47:35 +0100

    [feat]: limit cmp window

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set pumheight=10")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
