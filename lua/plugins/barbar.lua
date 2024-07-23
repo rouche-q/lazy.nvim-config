9fec1ca88ca96178a1d906f6626114022e0c8547
rouche-q <17704187+rouche-q@users.noreply.github.com>
2024-07-23 13:46:10 +0200

    [fix]: remove sidebar_filtype in barbar

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -7,9 +7,6 @@ return {
   init = function() vim.g.barbar_auto_setup = true end,
   opts = {
     auto_hide = 1,
-    sidebar_filetypes = {
-      ['neo-tree'] = { event = 'BufWipeout' },
-    },
     exclude_ft = { 'qf' },
     icons = {},
     no_name_title = 'New file'
