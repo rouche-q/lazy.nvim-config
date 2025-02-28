3561ac1cc5840d07efa72ad126875f21e24ae3ab
rouche-q <17704187+rouche-q@users.noreply.github.com>
2025-02-28 22:19:44 +0100

    feat(lua/plugins): add more snippets and autoclose plugin

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,8 +1,21 @@
 return {
   'saghen/blink.cmp',
-  dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = {
+    {
+      'L3MON4D3/LuaSnip',
+      version = 'v2.*',
+      dependencies = {
+        'rafamadriz/friendly-snippets',
+        config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+            require('luasnip.loaders.from_vscode').lazy_load({ paths = { vim.fn.stdpath 'config' .. '/snippets' } })
+          end
+      }
+    },
+  },
   opts = {
     keymap = { preset = 'enter' },
+    snippets = { preset = 'luasnip' },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
